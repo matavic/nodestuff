91ec87e4073a1505e5ac7246feabfc32b4b3e53e
Ubuntu <ubuntu@ubuntu-xenial.localdomain>
2017-03-10 20:31:07 +0000

    Third commit - Before add express framework

---
+++ webclient.js
@@ -0,0 +1,24 @@
+var http = require('http')
+
+//Options to be used by request
+var options = {
+  host : 'localhost',
+  port : '8081',
+  path : '/index.html'
+}
+
+// Callback function is used to deal with response
+var callback = function(response){
+  // Continously update stream with data
+  var body = ''
+  response.on('data', function(data){
+    body += data
+  })
+  response.on('end', function(){
+    // Data received completely
+    console.log(body)
+  })
+}
+// Make a request to the Server
+var req = http.request(options, callback)
+req.end()
