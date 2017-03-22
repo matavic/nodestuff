336dd4eeb46104411605ab6d83affd2558fb2a78
Vicente Mata <matavic25@hotmail.com>
2017-03-21 21:53:07 -0400

    21 03 2017

---
+++ socketioserver.js
@@ -0,0 +1,22 @@
+var http = require('http'), fs = require('fs')
+var IOServer = require('socket.io')
+
+function handler(request, response) {
+  var index = fs.readFileSync('index5.html')
+  index = index.toString()
+  response.writeHead(200, {'Content-Type':'text/html',
+                           'Content-Length':Buffer.byteLength(index)
+  })
+  response.end(index)
+}
+
+var server = http.createServer(handler)
+var io = new IOServer(server)
+
+io.on('connection', function (socket){
+  //console.log('New Connection');
+  socket.on('message', function(message){
+    socket.send(message)
+  })
+})
+server.listen(8081)
